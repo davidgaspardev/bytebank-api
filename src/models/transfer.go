a29b88beda554eb92aa6058c0e38872c4841208b
David Gaspar <davidgaspar.dev@gmail.com>
2021-10-30 20:32:19 +0000

    Transfer model added

---
+++ src/models/transfer.go
@@ -0,0 +1,11 @@
+package model
+
+type Transfer struct {
+	Id      string  `json:"id,omitempty" bson:"_id,omitempty"`
+	Value   float32 `json:"value,omitempty" bson:"value,omitempty"`
+	Contact struct {
+		Name          string `json:"name,omitempty" bson:"name,omitempty"`
+		AccountNumber int    `json:"accountNumber,omitempty" bson:"accountNumber,omitempty"`
+	} `json:"contact,omitempty" bson:"value,omitempty"`
+	DateTime string `json:"dateTime,omitempty" bson:"dateTime,omitempty"`
+}
