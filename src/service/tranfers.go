2c1dcfe4fda7e02078741b26a92634af9e2d9964
David Gaspar <davidgaspar.dev@gmail.com>
2021-10-31 03:03:48 +0000

    Transfer service created

---
+++ src/service/tranfers.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	database "bytebank-api/src/database"
+	model "bytebank-api/src/models"
+	"encoding/json"
+	"log"
+)
+
+func AddTransfer(transfer model.Transfer) (string, error) {
+	var object_id, err = database.AddData("cardflix", transfer)
+	if err != nil {
+		return "", err
+	}
+	return object_id, nil
+}
+
+func GetAllTransfers() []model.Transfer {
+	var data = database.GetAllData("cardflix")
+
+	var result = make([]model.Transfer, len(data))
+
+	for i, d := range data {
+		var dataJson, err = json.Marshal(d)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		json.Unmarshal(dataJson, &result[i])
+	}
+
+	return result
+}
